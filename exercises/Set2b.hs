e4a25bb107a5b3a2590892a4563ca6ef5b3742cb
Ekaum Singh <e4singh@uwaterloo.ca>
2026-04-19 12:12:15 -0400

    ex 1 & 2

--- exercises/Set2b.hs
+++ exercises/Set2b.hs
@@ -16,7 +16,9 @@ import Data.List
 -- Hint! pattern matching is your friend.
 
 binomial :: Integer -> Integer -> Integer
-binomial = todo
+binomial n 0 = 1
+binomial 0 k = 0
+binomial n k = binomial (n-1) k + binomial (n-1) (k-1)
 
 ------------------------------------------------------------------------------
 -- Ex 2: implement the odd factorial function. Odd factorial is like
@@ -27,7 +29,10 @@ binomial = todo
 --   oddFactorial 6 ==> 5*3*1 ==> 15
 
 oddFactorial :: Integer -> Integer
-oddFactorial = todo
+oddFactorial n
+  | n == 1 = 1
+  | n `mod` 2 == 0 = oddFactorial (n-1)
+  | otherwise = n * oddFactorial (n-2)
 
 ------------------------------------------------------------------------------
 -- Ex 3: implement the Euclidean Algorithm for finding the greatest
@@ -58,8 +63,22 @@ oddFactorial = todo
 -- Background reading:
 -- * https://en.wikipedia.org/wiki/Euclidean_algorithm
 
+-- myGcd :: Integer -> Integer -> Integer
+-- myGcd a 0 = a
+-- myGcd 0 b = b
+-- myGcd a b = if a >= b then myGcd (a-b) b else myGcd a (b-a)
+
 myGcd :: Integer -> Integer -> Integer
-myGcd = todo
+myGcd 0 y = y
+myGcd x y = if xAbs < yAbs
+            then myGcd yAbs xAbs
+            else myGcd (xAbs-yAbs) yAbs
+  -- Using the absolute values of x and y makes this function work
+  -- even with negative inputs. This is not required for this set.
+  -- However, without this fix you can run into problems in Set 6,
+  -- if you reuse this answer :)
+  where xAbs = abs x
+        yAbs = abs y
 
 ------------------------------------------------------------------------------
 -- Ex 4: Implement the function leftpad which adds space characters
@@ -75,7 +94,7 @@ myGcd = todo
 -- * you can compute the length of a string with the length function
 
 leftpad :: String -> Int -> String
-leftpad = todo
+leftpad str len = if length str >= len then str else leftpad (" " ++ str) len
 
 ------------------------------------------------------------------------------
 -- Ex 5: let's make a countdown for a rocket! Given a number, you
@@ -91,12 +110,12 @@ leftpad = todo
 -- * you'll probably need a recursive helper function
 
 countdown :: Integer -> String
-countdown = todo
+countdown n = "Ready! " ++ helper2 n ++ "Liftoff!" 
 
-------------------------------------------------------------------------------
--- Ex 6: implement the function smallestDivisor that returns the
--- smallest number (greater than 1) that divides the given number evenly.
---
+helper2 :: Integer -> String
+helper2 0 = ""
+helper2 n = show n ++ "... " ++ helper2 (n-1) 
+------------------------------------------------------------------------------ -- Ex 6: implement the function smallestDivisor that returns the -- smallest number (greater than 1) that divides the given number evenly.  --
 -- That is, when
 --   smallestDivisor n ==> k
 -- we have
@@ -109,7 +128,10 @@ countdown = todo
 -- Hint: remember the mod function!
 
 smallestDivisor :: Integer -> Integer
-smallestDivisor = todo
+smallestDivisor n = helper n 2
+
+helper :: Integer -> Integer -> Integer
+helper n x = if n `mod` x == 0 then x else helper n (x+1) 
 
 ------------------------------------------------------------------------------
 -- Ex 7: implement a function isPrime that checks if the given number
@@ -118,7 +140,9 @@ smallestDivisor = todo
 -- Ps. 0 and 1 are not prime numbers
 
 isPrime :: Integer -> Bool
-isPrime = todo
+isPrime 0 = False
+isPrime 1 = False
+isPrime n = smallestDivisor n == n
 
 ------------------------------------------------------------------------------
 -- Ex 8: implement a function biggestPrimeAtMost that returns the
@@ -133,4 +157,4 @@ isPrime = todo
 --   biggestPrimeAtMost 10 ==> 7
 
 biggestPrimeAtMost :: Integer -> Integer
-biggestPrimeAtMost = todo
+biggestPrimeAtMost n = if isPrime n then n else biggestPrimeAtMost (n-1)
